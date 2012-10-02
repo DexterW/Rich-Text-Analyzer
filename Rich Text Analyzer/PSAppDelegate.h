197a572e75b07e1ef4ceeebedde02c796f94905e
Dexter Weiss <dexter.weiss@gmail.com>
2012-10-01 20:40:40 -0500

    Initial Commit

---
+++ Rich Text Analyzer/PSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PSAppDelegate.h
+//  Rich Text Analyzer
+//
+//  Created by Dexter Weiss on 10/1/12.
+//  Copyright (c) 2012 Peppered Software. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface PSAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Rich Text Analyzer/PSAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  PSAppDelegate.m
+//  Rich Text Analyzer
+//
+//  Created by Dexter Weiss on 10/1/12.
+//  Copyright (c) 2012 Peppered Software. All rights reserved.
+//
+
+#import "PSAppDelegate.h"
+
+@implementation PSAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
